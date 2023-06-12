045dd30be73c29194aacd6c38e67176705d41742
One_Ordinary_Man <srivathsansudarsanan@outlook.com>
2023-06-12 12:15:31 +0530

    Prototype is complete.

---
+++ src/lib/lib.h
@@ -0,0 +1,25 @@
+#ifndef LIB_H
+#define LIB_H
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+  int value;
+  struct Node *next;
+};
+typedef struct Node node;
+typedef struct {
+  int size;
+  node head;
+  node tail;
+} linked_list;
+linked_list create_list();
+node *create_node();
+void prepend(linked_list *, int);
+void append(linked_list *, int);
+int get_index(linked_list *, int);
+int get_value(linked_list *, int);
+int remove_head(linked_list *);
+void insert_at_index(linked_list *, int);
+int remove_at_index(linked_list *, int);
+void delete_list(linked_list *);
+#endif
