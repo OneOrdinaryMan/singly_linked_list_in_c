972b6a72b44ba9c956a62182fdd32c2c858fcbf5
One_Ordinary_Man <srivathsansudarsanan@outlook.com>
2023-06-12 14:26:27 +0530

    Added the linked list in main.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,12 @@
+#include "lib/lib.h"
 #include <stdio.h>
 int main() {
-  printf("Hello, World!\n");
+  linked_list input = create_list();
+  for (int i = 1; i < 10; i++) {
+    append(&input, i);
+  }
+  printf("%d\n", get_index(&input, 4));
+  printf("%d\n", get_value(&input, 0));
+  delete_list(&input);
   return 0;
 }
